2ba3b5a6e7fa25d60e7563fac27848b3da7304ba
KallDrexx <me@mshapiro.net>
2025-03-05 23:02:29 -0500

    Removed simple macro attribute
    This attribute was replaced by the `CustomFunctionAttribute` that's now
    built into dntc.

--- src/DotnetSnes.Core/CUtils.cs
+++ src/DotnetSnes.Core/CUtils.cs
@@ -1,3 +1,4 @@
+using Dntc.Attributes;
 using DotnetSnes.Core.TranspilerSupport;
 
 namespace DotnetSnes.Core;
@@ -7,7 +8,7 @@ public static class CUtils
     /// <summary>
     /// Gets the count of bytes between two addresses
     /// </summary>
-    [SimpleMacro("bytesBetween", "#define bytesBetween(a,b) ((a) - (b))")]
+    [CustomFunction("#define bytesBetween(a,b) ((a) - (b))", null, "bytesBetween")]
     public static byte BytesBetweenAddress(ref byte first, ref byte second)
     {
         return 0;
@@ -19,7 +20,7 @@ public static class CUtils
     /// Needed because C# requires the `fixed` keyword, and that compiles to extra MSIL. So this
     /// allows us to compile it down directly to a simple macro
     /// </summary>
-    [SimpleMacro("pointerTo", "#define pointerTo(a) (&(a))")]
+    [CustomFunction("#define pointerTo(a) (&(a))", null, "pointerTo")]
     public static unsafe T* AddressOf<T>(T obj)
     {
         return null;
@@ -28,7 +29,7 @@ public static class CUtils
     /// <summary>
     /// Gets a `void *` pointer to referenced variable.
     /// </summary>
-    [SimpleMacro("voidPointer", "#define voidPointer(a) ((char *)(&(a)))")]
+    [CustomFunction("#define voidPointer(a) ((char *)(&(a)))", null, "voidPointer")]
     public static unsafe char* GetCharPointer<T>(T variableName)
     {
         return null;
